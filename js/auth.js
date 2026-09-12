05d81a2c0d3af92c1bc78d03f25e1ae28504e288
Voltyx Studio <fourmisdesamis@gmail.com>
2026-09-12 22:58:05 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -8,15 +8,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ⚠️ Remplace par TA config Firebase (console Firebase → Paramètres du projet)
+// ⚠️ TA config Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBSCqSsBTXk9Q8sBX88NgrdDHUAHT0Cq6I",
   authDomain: "lumalia.firebaseapp.com",
-  projectId: "lumalia",
+  projectId: "lumania",
   storageBucket: "lumalia.firebasestorage.app",
   messagingSenderId: "189011821397",
-  appId: "1:189011821397:web:03c8609d35d488dce2a5dc",
-  measurementId: "G-4G4KMZCWFH"
+  appId: "1:189011821397:web:03c8609d35d488dce2a5dc"
 };
 
 const app = initializeApp(firebaseConfig);
