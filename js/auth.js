68a1c4a57c0023f7aefe8f5be449c3839dcf6667
Voltyx Studio <fourmisdesamis@gmail.com>
2026-09-12 22:37:11 +0200

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+// js/auth.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// ⚠️ Remplace par TA config Firebase (console Firebase → Paramètres du projet)
+const firebaseConfig = {
+  apiKey: "AIzaSyBSCqSsBTXk9Q8sBX88NgrdDHUAHT0Cq6I",
+  authDomain: "lumalia.firebaseapp.com",
+  projectId: "lumalia",
+  storageBucket: "lumalia.firebasestorage.app",
+  messagingSenderId: "189011821397",
+  appId: "1:189011821397:web:03c8609d35d488dce2a5dc",
+  measurementId: "G-4G4KMZCWFH"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+};
